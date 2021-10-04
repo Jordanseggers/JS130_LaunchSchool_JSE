ad33b03e78a032822a995b6fb33a73c65acac058
JordanSEggers <smrsmd@hotmail.com>
2021-10-04 18:22:47 +0000

    eslint

--- Challenges/Medium/Round1/01_Diamond.js
+++ Challenges/Medium/Round1/01_Diamond.js
@@ -1,28 +1,28 @@
-class Diamond {};
+class Diamond {}
 
 Diamond.makeDiamond = function(letter) {
   let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
   let lettersInTypRow = 2;
   let letterIdx = alphabet.findIndex(char => char === letter);
-  let widthAndHeight = letterIdx * 2 + 1;
+  let widthAndHeight = (letterIdx * 2) + 1;
   let diamondTop = [];
   let spacesBetween = 1;
-  
+
   diamondTop.push(" ".repeat(Math.floor(widthAndHeight / 2)) + "A" + " ".repeat(Math.floor(widthAndHeight / 2)));
-  if(letterIdx < 1){
+  if (letterIdx < 1) {
     return diamondTop.join("") + "\n";
   }
-  
-  for(let i = 1; i <= letterIdx; i++){
-    let spacesOutside = (widthAndHeight - lettersInTypRow - spacesBetween)/2;
-    diamondTop.push(" ".repeat(spacesOutside) + alphabet[i] + " ".repeat(spacesBetween) + alphabet[i] + " ".repeat(spacesOutside));
+
+  for (let idx = 1; idx <= letterIdx; idx++) {
+    let spacesOutside = (widthAndHeight - lettersInTypRow - spacesBetween) / 2;
+    diamondTop.push(" ".repeat(spacesOutside) + alphabet[idx] + " ".repeat(spacesBetween) + alphabet[idx] + " ".repeat(spacesOutside));
     spacesBetween += 2;
   }
-  
+
   let diamondBottom = diamondTop.slice(0, letterIdx).reverse();
-  
+
   return diamondTop.join("\n") + "\n" + diamondBottom.join("\n") + "\n";
-}
+};
 
 console.log(Diamond.makeDiamond("A"));
 
--- Challenges/Medium/Round1/03_Clock.js
+++ Challenges/Medium/Round1/03_Clock.js
@@ -6,81 +6,81 @@ class Clock {
     this.hour = hour;
     this.min = min;
   }
-  
+
   add (min) {
     let newMin = this.min + min;
     let hoursToAdd = (Math.floor(newMin / MIN_PER_HOUR));
-    newMin = newMin - (60 * hoursToAdd);
+    newMin -= (60 * hoursToAdd);
     this.min = newMin;
     this.addHour(hoursToAdd);
     return this;
   }
-  
+
   addHour (numOfHours) {
     let newHour = this.hour + numOfHours;
-    while(newHour > 24) {
+    while (newHour > 24) {
       newHour -= 24;
     }
-    if(newHour === 24){
-      if(this.min > 0){
+    if (newHour === 24) {
+      if (this.min > 0) {
         newHour = 0;
       }
     }
     this.hour = newHour;
   }
-  
+
   subtract (min) {
     let hoursToSubtract = Math.floor(min / MIN_PER_HOUR);
     let minToSubtract = min - (hoursToSubtract * MIN_PER_HOUR);
-    if(this.min === 0 && minToSubtract > 0){
+    if (this.min === 0 && minToSubtract > 0) {
       this.min = 60;
       hoursToSubtract += 1;
     }
     let newMin = this.min - minToSubtract;
-    
-    if(newMin < 0){
+
+    if (newMin < 0) {
       hoursToSubtract += 1;
     }
     this.min = Math.abs(newMin);
     this.subtractHours(hoursToSubtract);
     return this;
   }
-  
-  subtractHours(numOfHours){
+
+  subtractHours(numOfHours) {
     let newHours = this.hour - numOfHours;
     console.log(newHours);
-    if(newHours >= 0) {
+    if (newHours >= 0) {
       this.hour = newHours;
     } else if (newHours > -24 && newHours < 0) {
       this.hour = 24 + newHours;
-    } else if (newHours < -24){
+    } else if (newHours < -24) {
       let posNewHours = Math.abs(newHours);
-      while(posNewHours > 24){
+      while (posNewHours > 24) {
         posNewHours -= 24;
       }
       this.hour = 24 - posNewHours;
     }
   }
-  
-  toString(){
+
+  toString() {
     let time = "";
-    
-    if(this.hour <= 9) {
+
+    if (this.hour <= 9) {
       time += `0${this.hour}`;
     } else {
       time += `${this.hour}`;
     }
-    
-    if(this.min <= 9){
+
+    if (this.min <= 9) {
       time += `:0${this.min}`;
     } else {
       time += `:${this.min}`;
     }
     return time;
   }
-  
-  isEqual(clockObj){
-    if(this.hour === clockObj.hour && this.min === clockObj.min){
+
+  isEqual(clockObj) {
+    if (this.hour === clockObj.hour && this.min === clockObj.min) {
       return true;
     } else {
       return false;
@@ -92,7 +92,5 @@ Clock.at = function (hour, min) {
   return new Clock(hour, min);
 };
 
-let hey = Clock.at(0).subtract(50);
-console.log(hey.toString());
 
 module.exports = { Clock, MIN_PER_HOUR, HOUR_PER_DAY };
